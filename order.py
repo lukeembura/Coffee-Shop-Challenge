3cf3c498c69987f3ec4d4b7af3cb29dd86e99201
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-05-17 18:46:31 +0300

    Updated order.py

--- order.py
+++ order.py
@@ -1,2 +1,8 @@
 class Order:
     all_orders = []
+
+    def __init__(self, coffee, customer, price):
+        if not isinstance(customer, Customer):
+            raise Exception("customer must be a Customer instance.")
+        if not isinstance(coffee, Coffee):
+            raise Exception("coffee must be a Coffee instance.")
