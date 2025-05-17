c4bac8b8933e479bf52081aeee553ec7b738fecb
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-05-17 18:37:24 +0300

    Updated coffee.py

--- coffee.py
+++ coffee.py
@@ -1,6 +1,18 @@
 class Coffee:
-    def __init__(self, name)
-        if ininstance(name, str) and len(name) >= 3:
+    def __init__(self, name):
+        if isinstance(name, str) and len(name) >= 3:
             self._name = name
         else:
             raise Exception("Coffee name must be a string with at least 3 characters.")
+        
+    @property
+    def name(self):
+        return self._name
+    
+    def  orders(self):
+        return [order for order in Order.all_orders if order.coffee == self]
+    
+    def customers(self):
+        return list({order.customer for order in self.orders()})
+    
+    
