c6a98d707bee6f943c2a94da6156b16af645ea6b
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-05-18 11:16:01 +0300

    Update changes

--- customer.py
+++ customer.py
@@ -5,31 +5,26 @@ class Customer:
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, value):
-        if isinstance(value, str) and 1 <= len(value) <= 15:
-            self._name = value
-        else:
-            raise Exception("Name must be a string between 1 and 15 characters.")
+        if not isinstance(value, str) or not (1 <= len(value) <= 15):
+            raise Exception("Name must be a string between 1 and 15 characters")
+        self._name = value
 
     def orders(self):
-        return [order for order in Order.all_orders if order.customer == self]
-    
+        return [order for order in Order.all if order.customer == self]
+
     def coffees(self):
-        return list({order.coffee for order in self.orders()})
-    
+        return list(set(order.coffee for order in self.orders()))
+
     def create_order(self, coffee, price):
         return Order(self, coffee, price)
-    
+
     @classmethod
-    def most_aficianado(cls, coffee):
-        top_customer = None
-        max_orders = 0
-        for customer in {order.customer for order in coffee.orders()}:
-            total = sum(order.price for order in customer.orders() if order.coffee == coffee)
-            if total > max_spent:
-                max_spent = total
-                top_customer = customer
-        return top_customer
+    def most_aficionado(cls, coffee):
+        customer_spending = {}
+        for order in coffee.orders():
+            customer_spending[order.customer] = customer_spending.get(order.customer, 0) + order.price
+        return max(customer_spending, key=customer_spending.get) if customer_spending else None
 
