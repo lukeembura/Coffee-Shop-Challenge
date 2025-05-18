706855aa757b33decc99c1c337b1e3daaf024143
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-05-18 11:17:10 +0300

    Update changes

--- order.py
+++ order.py
@@ -1,28 +1,27 @@
 class Order:
-    all_orders = []
+    all = []
 
-    def __init__(self, coffee, customer, price):
+    def __init__(self, customer, coffee, price):
         if not isinstance(customer, Customer):
-            raise Exception("customer must be a Customer instance.")
+            raise Exception("customer must be a Customer instance")
         if not isinstance(coffee, Coffee):
-            raise Exception("coffee must be a Coffee instance.")
-        if not (isinstance(price, float) and 1.0 <= price <= 10.0):
-            raise Exception("price must be a float between 1.0 and 10.0.")
-        
+            raise Exception("coffee must be a Coffee instance")
+        if not isinstance(price, float) or not (1.0 <= price <= 10.0):
+            raise Exception("price must be a float between 1.0 and 10.0")
+
         self._customer = customer
         self._coffee = coffee
         self._price = price
-        Order.all_orders.append(self)
+        Order.all.append(self)
 
     @property
     def customer(self):
         return self._customer
-    
+
     @property
     def coffee(self):
         return self._coffee
-    
+
     @property
     def price(self):
         return self._price
-        
